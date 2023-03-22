80138573752bd481d2ece5f6f477d172994b39fc
loveispanec83 <loveispanec83@gmail.com>
2023-03-22 15:15:46 +0200

    burger adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -56,14 +56,33 @@ const swiper = new Swiper(".reviews__slider", {
   },
 });  
   
-  
+ //burger
 
 });
 
-// $(function () {
-//   $(".reviews-slider").slick({
-//     dots: true,
-//     arrows: true,
-//   });
-  
-// });
+function burgerMenu(selector) {
+  let menu = $(selector);
+  let button = menu.find(".burger-menu_button", ".burger-menu_lines");
+  let links = menu.find(".burger-menu_link");
+  let overlay = menu.find(".burger-menu_overlay");
+
+  button.on("click", (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
+
+  links.on("click", () => toggleMenu());
+  overlay.on("click", () => toggleMenu());
+
+  function toggleMenu() {
+    menu.toggleClass("burger-menu_active");
+
+    if (menu.hasClass("burger-menu_active")) {
+      $("body").css("overlow", "hidden");
+    } else {
+      $("body").css("overlow", "visible");
+    }
+  }
+}
+
+burgerMenu(".burger-menu");
